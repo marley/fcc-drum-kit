de7bb2eb8695e07828c13d40f88bfe210e1b313a
Marley Alford <marleycalford@gmail.com>
2021-04-10 16:34:32 -0400

    Fixed test #6

--- src/App.js
+++ src/App.js
@@ -7,45 +7,54 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      drumpadData: {
-        81: {
+      drumpadData: [
+        {
+          keyCode: 81,
           name: "Q",
           sound:
             "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3",
         },
-        87: {
+        {
+          keyCode: 87,
           name: "W",
           sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
         },
-        69: {
+        {
+          keyCode: 69,
           name: "E",
           sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
         },
-        65: {
+        {
+          keyCode: 65,
           name: "A",
           sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
         },
-        83: {
+        {
+          keyCode: 83,
           name: "S",
           sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
         },
-        68: {
+        {
+          keyCode: 68,
           name: "D",
           sound: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
         },
-        90: {
+        {
+          keyCode: 90,
           name: "Z",
           sound: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
         },
-        88: {
+        {
+          keyCode: 88,
           name: "X",
           sound: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
         },
-        67: {
+        {
+          keyCode: 67,
           name: "C",
           sound: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
         },
-      },
+      ],
       lastPlayed: {},
     };
     this.handleKeyDown = this.handleKeyDown.bind(this);
@@ -53,12 +62,21 @@ class App extends React.Component {
   }
 
   handleKeyDown(event) {
-    if (this.state.drumpadData.hasOwnProperty(event.keyCode)) {
+    let keyIdx = this.state.drumpadData.reduce(
+      (triggerKeyIdx, drumpadObj, i) => {
+        if (drumpadObj["keyCode"] === event.keyCode) {
+          triggerKeyIdx = i;
+        }
+        return triggerKeyIdx;
+      },
+      -1
+    );
+    if (keyIdx >= 0) {
       const sound = document.getElementById(
-        `${this.state.drumpadData[event.keyCode]["name"]}`
+        `${this.state.drumpadData[keyIdx]["name"]}`
       );
       sound.play();
-      this.refreshDisplay(this.state.drumpadData[event.keyCode]);
+      this.refreshDisplay(this.state.drumpadData[keyIdx]);
     }
   }
 
